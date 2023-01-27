1a2844d0470a2207b38bf144a29c81852216b47d
JanineReidenbach <119943084+JanineReidenbach@users.noreply.github.com>
2023-01-27 08:05:50 +0100

    Create GitTest.c

---
+++ GitTest.c
@@ -0,0 +1,11 @@
+// Git-Test
+
+#include<stdio.>
+
+int main() {
+  
+    printf("\n Dies ist ein einfacher Test, um Git zu initialisieren und etwas zu pullen. \n Check: :-) \n");
+
+    return 0;
+
+}
